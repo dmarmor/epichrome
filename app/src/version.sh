40d5e0b3758bb441f1b6522bd35e31e8b9af1a5c
David Marmor <dmarmor@gmail.com>
2020-08-15 12:13:50 -0700

    Committing version.sh prior to branch switch.

--- app/src/version.sh
+++ app/src/version.sh
@@ -1,7 +1,7 @@
 # Epichrome version (#.#.# = release version, #.#.#b# = beta)
 
 epiVersion=2.4.0b1
-epiBuildNum=84
+epiBuildNum=117
 epiDesc="🚀 MAJOR UPDATE!
 
    ▪️ Added ability to edit existing Epichrome apps
