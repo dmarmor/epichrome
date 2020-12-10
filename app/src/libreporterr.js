0950fd6eff398efd539e02b8c1f4499f9908065c
David Marmor <dmarmor@gmail.com>
2020-12-10 11:23:03 -0800

    Added code to detect and handle too-long GitHub URL in libreporterr.js.

--- app/src/libreporterr.js
+++ app/src/libreporterr.js
@@ -19,6 +19,14 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+const kMaxUrlLength = 8196;
+
+const kUrlStart = 'https://github.com/dmarmor/epichrome/issues/new?title=';
+const kUrlBodyTag = '&body=';
+const kBodyStart = '<!-- ℹ️ Please provide as much detail as you can about how this error occurred. -->';
+const kLogPre = "\n\n<!-- ℹ️ Below is a log of this run of Epichrome, which may be helpful in diagnosing this error. Please redact any paths or information you're not comfortable sharing before posting this issue. -->\n\n```\n";
+const kLogPost = '\n```';
+
 
 // REPORTERROR: report an error to GitHub
 function reportError(aTitle, aLogFile=undefined) {
@@ -26,7 +34,8 @@ function reportError(aTitle, aLogFile=undefined) {
     let iResult, iErr;
     
     // get default log file if we have one
-    if ((aLogFile === undefined) && gCoreInfo && gCoreInfo.logFile) {
+    if ((aLogFile === undefined) && (typeof gCoreInfo !== 'undefined') &&
+            gCoreInfo && gCoreInfo.logFile) {
         aLogFile = gCoreInfo.logFile;
     }
     
@@ -36,14 +45,30 @@ function reportError(aTitle, aLogFile=undefined) {
         aLogFile = Path(aLogFile);
         try {
             iLog = kApp.read(aLogFile);
-            iLog = "\n\n[Below is a log of this run of Epichrome, which may be helpful in diagnosing this error. Please redact any paths or information you're not comfortable sharing before posting this issue.]\n\n```\n" + iLog + '\n```'
         } catch(iErr) {} // fail silently
     }
     
     // open a GitHub issue page populated with our error info
+    let iUrlStart = kUrlStart + encodeURIComponent(aTitle) + kUrlBodyTag + encodeURIComponent(kBodyStart);
+    let iUrl = iUrlStart;
+    
     try {
-        kApp.openLocation('https://github.com/dmarmor/epichrome/issues/new?title=' +
-        encodeURIComponent(aTitle) + '&body=' + encodeURIComponent('[Please provide as much detail as you can about how this error occurred.]' + iLog));
+        
+        // add in log
+        if (iLog) {
+            
+            // ensure URL isn't too long with log
+            iUrl += encodeURIComponent(kLogPre + iLog + kLogPost);
+            
+            // if too long, ask for it to be manually pasted in
+            if (iUrl.length >= kMaxUrlLength) {
+                iUrl = iUrlStart + encodeURIComponent("\n\n<!-- ❗ The log file is too long to include in the URL. To help diagnose this error, please copy the log and paste the log in the area below. Please redact any paths or information you're not comfortable sharing before posting this issue. -->\n\n```\n[ℹ️ PASTE LOG HERE]\n```");
+            }
+        }
+        
+        // open URL
+        kApp.openLocation(iUrl);
+        
     } catch(iErr) {
         let iErrMsg = 'Unable to open GitHub issues page. (' + iErr.message + ')';
         if (typeof dialog === "function") {
