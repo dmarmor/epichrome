06b0f95cdec54656d10d99ad5ae8bce4fef6b136
David Marmor <dmarmor@gmail.com>
2021-03-15 10:59:25 -0700

    Factored version code into function in versioncode.sh for possible later move into core.sh.

--- app/src/versioncode.sh
+++ app/src/versioncode.sh
@@ -2,13 +2,35 @@
 
 # <maj><02-min><03-bug>.<03-beta|100>.<build|10000>
 
-vre='^0*([0-9]+)\.0*([0-9]+)\.0*([0-9]+)(b0*([0-9]+))?(\[0*([0-9]+)])?$'
-if [[ "$1" =~ $vre ]] ; then
-    vbeta="${BASH_REMATCH[5]}" ; [[ "$vbeta" ]] || vbeta='100'
-    vbuild="${BASH_REMATCH[7]}" ; [[ "$vbuild" ]] || vbuild='10000'
-    printf '%d.%d.%d\n' \
-	   $(( (${BASH_REMATCH[1]} * 100000) + (${BASH_REMATCH[2]} * 1000) + ${BASH_REMATCH[3]} )) \
-	   $vbeta $vbuild
-else
+if ! source src/core.sh 2> /dev/null ; then
+    echo '*** Unable to load src/core.sh' 1>&2
+    exit 1
+fi
+logNoStderr=1
+
+# VCODE -- echo numeric code for given version (current if none given)
+#   vcode([aVersion aBuildNum])
+function vcode {
+    
+    # argument
+    local aVersion="$1" ; shift ; [[ "$aVersion" ]] || aVersion="$coreVersion"
+    local aBuildNum="$1" ; shift
+    
+    if [[ "$aVersion" =~ ^$epiVersionRe$ ]] ; then
+        local iBeta="${BASH_REMATCH[5]}" ; [[ "$iBeta" ]] || iBeta='100'
+        if [[ ! "$aBuildNum" ]] ; then
+            aBuildNum="${BASH_REMATCH[7]}" ; [[ "$aBuildNum" ]] || aBuildNum='10000'
+        fi
+        printf '%d.%d.%d\n' \
+                $(( (${BASH_REMATCH[1]} * 100000) + (${BASH_REMATCH[2]} * 1000) + ${BASH_REMATCH[3]} )) \
+                $iBeta $aBuildNum
+    else
+        return 1
+    fi
+}
+
+if ! vcode "$1" ; then
     echo "FAIL"
 fi
+
+cleanexit
