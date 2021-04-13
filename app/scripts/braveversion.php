8744871d0b6af1d2b9a927ed00a38c169c92d0ee
David Marmor <dmarmor@gmail.com>
2021-04-12 21:29:42 -0700

    Added Makefile rule and script to enable building with custom-built module version of Platypus compiled to create universal apps.

--- app/scripts/braveversion.php
+++ app/scripts/braveversion.php
@@ -1,6 +1,26 @@
 #!/usr/bin/php
 <?php
 
+// braveversion.php: PHP script for finding the latest version of Brave at brave.com
+//
+// Copyright (C) 2021  David Marmor
+//
+// https://github.com/dmarmor/epichrome
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 // turn off the many warnings emitted when reading HTML
 error_reporting(error_reporting() & ~E_WARNING);
 
--- app/scripts/updatebrave.sh
+++ app/scripts/updatebrave.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+#
+#  updatebrave.sh: check for a new version of Brave, download, and manage old versions
+#
+#  Copyright (C) 2021  David Marmor
+#
+#  https://github.com/dmarmor/epichrome
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 
 # URLs
 latestUrl='https://brave.com/latest/'
